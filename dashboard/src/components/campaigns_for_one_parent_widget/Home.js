9e5ce28fcaf842b2ca917513d02da75be751c01e
Brian Haller <brianshaller@gmail.com>
2018-11-14 12:32:19 -0800

    commit

--- dashboard/src/components/campaigns_for_one_parent_widget/Home.js
+++ dashboard/src/components/campaigns_for_one_parent_widget/Home.js
@@ -68,6 +68,7 @@ class Home extends Component {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
+            'x-auth': localStorage.getItem('token'),
           },
           body: JSON.stringify({
             dateRange: this.state.dateRange,
