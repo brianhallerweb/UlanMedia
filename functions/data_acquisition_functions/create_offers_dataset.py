0a8b68206e1f8a432b63c068b65a4e59df14b3bc
Brian Haller <brianshaller@gmail.com>
2018-12-15 22:03:53 -0700

    commit

--- functions/data_acquisition_functions/create_offers_dataset.py
+++ functions/data_acquisition_functions/create_offers_dataset.py
@@ -8,9 +8,13 @@ from functions.misc.send_email import send_email
 
 def create_offers_dataset(token, date_range, vol_start_date, vol_end_date):
     try:
-        url = f"https://api.voluum.com/report?from={vol_start_date}T00%3A00%3A00Z&to={vol_end_date}T00%3A00%3A00Z&tz=America%2FLos_Angeles&conversionTimeMode=VISIT&sort=offerName&direction=desc&columns=offerName&columns=campaignName&columns=visits&columns=conversions&columns=revenue&columns=cost&columns=profit&columns=cpv&columns=cv&columns=roi&columns=epv&columns=campaignId&columns=cpa&groupBy=offer&groupBy=campaign&offset=0&limit=1000&include=ACTIVE&filter1=traffic-source&filter1Value=37bbd390-ed90-4978-9066-09affa682bcc"
+        url = f"https://api.voluum.com/report?from={vol_start_date}T00%3A00%3A00Z&to={vol_end_date}T00%3A00%3A00Z&tz=America%2FLos_Angeles&conversionTimeMode=VISIT&sort=offerName&direction=desc&columns=offerName&columns=campaignName&columns=visits&columns=conversions&columns=revenue&columns=cost&columns=profit&columns=cpv&columns=cv&columns=roi&columns=epv&columns=campaignId&columns=cpa&groupBy=offer&groupBy=campaign&offset=0&limit=100000&include=ACTIVE&filter1=traffic-source&filter1Value=37bbd390-ed90-4978-9066-09affa682bcc"
         res = requests.get(url, headers = {"cwauth-token": token})
         res.raise_for_status()
+        if res.json()["totalRows"] != len(res.json()["rows"]):
+            # you need to throw an error here
+            print("problem")
+            sys.exit()
 
         offers = {"metadata":{"vol_start_date": vol_start_date,
                               "vol_end_date": vol_end_date
@@ -35,6 +39,9 @@ def create_offers_dataset(token, date_range, vol_start_date, vol_end_date):
                 offer_string_parts = list(res[0])
                 offer_name = offer_string_parts[1]
                 offer_flow = offer_string_parts[2]
+
+            if offer_flow == "(bin: unitedKingdom - English)":
+                print(offer_name)
             
             if offerID not in offers["data"][campaignID]:
                 offers["data"][campaignID][offerID] = {"clicks": row["visits"],
--- scripts/data_acquisition_scripts/create_offers_datasets.py
+++ scripts/data_acquisition_scripts/create_offers_datasets.py
@@ -5,41 +5,41 @@ from functions.misc.create_vol_date_range import create_vol_date_range
 
 vol_token = get_vol_access_token(vol_access_id, vol_access_key)
 
-#############################################
-# create a data set for yesterday 
-date_range = "yesterday"
-vol_dates = create_vol_date_range(1, mgid_timezone)
-vol_start_date = vol_dates[0]
-vol_end_date = vol_dates[1]
+# #############################################
+# # create a data set for yesterday 
+# date_range = "yesterday"
+# vol_dates = create_vol_date_range(1, mgid_timezone)
+# vol_start_date = vol_dates[0]
+# vol_end_date = vol_dates[1]
 
-create_offers_dataset(vol_token, date_range, vol_start_date,
-        vol_end_date)
+# create_offers_dataset(vol_token, date_range, vol_start_date,
+        # vol_end_date)
 
-print(f"{date_range} offers dataset created")
+# print(f"{date_range} offers dataset created")
 
-#############################################
-# create a data set for the last 7 days
-date_range = "seven"
-vol_dates = create_vol_date_range(7, mgid_timezone)
-vol_start_date = vol_dates[0]
-vol_end_date = vol_dates[1]
+# #############################################
+# # create a data set for the last 7 days
+# date_range = "seven"
+# vol_dates = create_vol_date_range(7, mgid_timezone)
+# vol_start_date = vol_dates[0]
+# vol_end_date = vol_dates[1]
 
-create_offers_dataset(vol_token, date_range, vol_start_date,
-        vol_end_date)
+# create_offers_dataset(vol_token, date_range, vol_start_date,
+        # vol_end_date)
 
-print(f"{date_range} offers dataset created")
+# print(f"{date_range} offers dataset created")
 
-#############################################
-# create a data set for the last 30 days
-date_range = "thirty"
-vol_dates = create_vol_date_range(30, mgid_timezone)
-vol_start_date = vol_dates[0]
-vol_end_date = vol_dates[1]
+# #############################################
+# # create a data set for the last 30 days
+# date_range = "thirty"
+# vol_dates = create_vol_date_range(30, mgid_timezone)
+# vol_start_date = vol_dates[0]
+# vol_end_date = vol_dates[1]
 
-create_offers_dataset(vol_token, date_range, vol_start_date,
-        vol_end_date)
+# create_offers_dataset(vol_token, date_range, vol_start_date,
+        # vol_end_date)
 
-print(f"{date_range} offers dataset created")
+# print(f"{date_range} offers dataset created")
 
 #############################################
 # create a data set for the last 90 days
@@ -54,13 +54,13 @@ create_offers_dataset(vol_token, date_range, vol_start_date,
 print(f"{date_range} offers dataset created")
 
 #############################################
-# create a data set for the last 180 days
-date_range = "oneeighty"
-vol_dates = create_vol_date_range(180, mgid_timezone)
-vol_start_date = vol_dates[0]
-vol_end_date = vol_dates[1]
+# # create a data set for the last 180 days
+# date_range = "oneeighty"
+# vol_dates = create_vol_date_range(180, mgid_timezone)
+# vol_start_date = vol_dates[0]
+# vol_end_date = vol_dates[1]
 
-create_offers_dataset(vol_token, date_range, vol_start_date,
-        vol_end_date)
+# create_offers_dataset(vol_token, date_range, vol_start_date,
+        # vol_end_date)
 
-print(f"{date_range} offers dataset created")
+# print(f"{date_range} offers dataset created")
