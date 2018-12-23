4ab37c1230ac8dd39339e863ede23b45353c36b6
Brian Haller <brianshaller@gmail.com>
2018-12-22 19:41:32 -0700

    commit

--- dashboard/src/components/GlobalNavBar.js
+++ dashboard/src/components/GlobalNavBar.js
@@ -5,37 +5,25 @@ import {NavLink} from 'react-router-dom';
 const GlobalNavBar = () => (
   <div className="globalNavBar">
     <li className="globalNavItem">
-      <NavLink
-        to={'/campaignsforallcampaigns'}
-        target="_blank"
-        activeClassName="is-active">
+      <NavLink to={'/campaignsforallcampaigns'} activeClassName="is-active">
         campaigns
       </NavLink>
     </li>
 
     <li className="globalNavItem">
-      <NavLink
-        to={'/pwidgetsforallcampaigns'}
-        target="_blank"
-        activeClassName="is-active">
+      <NavLink to={'/pwidgetsforallcampaigns'} activeClassName="is-active">
         p widgets
       </NavLink>
     </li>
 
     <li className="globalNavItem">
-      <NavLink
-        to={'/offersforallcampaigns'}
-        target="_blank"
-        activeClassName="is-active">
+      <NavLink to={'/offersforallcampaigns'} activeClassName="is-active">
         offers
       </NavLink>
     </li>
 
     <li className="globalNavItem">
-      <NavLink
-        to={'/adsforallcampaigns'}
-        target="_blank"
-        activeClassName="is-active">
+      <NavLink to={'/adsforallcampaigns'} activeClassName="is-active">
         ads
       </NavLink>
     </li>
