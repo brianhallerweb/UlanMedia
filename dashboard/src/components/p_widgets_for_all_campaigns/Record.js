0b4c4168e70a2793bb00e5005a5eadc3193c2029
Brian Haller <brianshaller@gmail.com>
2018-12-27 11:06:34 -0700

    commit

--- dashboard/src/components/p_widgets_for_all_campaigns/Record.js
+++ dashboard/src/components/p_widgets_for_all_campaigns/Record.js
@@ -8,6 +8,33 @@ class Record extends Component {
     this.state = {clicked: false};
   }
 
+  addTrainingData(decision) {
+    fetch('/api/pWidgetsForAllCampaignsTrainingData', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-auth': localStorage.getItem('token'),
+      },
+      body: JSON.stringify({
+        widgetID: this.props.widgetRecord.widget_id,
+        clicks: this.props.widgetRecord.clicks,
+        cost: this.props.widgetRecord.cost,
+        revenue: this.props.widgetRecord.revenue,
+        profit: this.props.widgetRecord.profit,
+        leads: this.props.widgetRecord.leads,
+        leadCPA: this.props.widgetRecord.lead_cpa,
+        leadCVR: this.props.widgetRecord.lead_cvr,
+        sales: this.props.widgetRecord.sales,
+        saleCPA: this.props.widgetRecord.sale_cpa,
+        globalStatusDecision: decision,
+      }),
+    }).then(res => {
+      if (!res.ok) {
+        throw Error(res.statusText);
+      }
+    });
+  }
+
   render() {
     return (
       <tr className={this.state.clicked && 'clicked'}>
@@ -66,6 +93,7 @@ class Record extends Component {
             <div onClick={() => this.setState({clicked: true})}>
               <div className="rowLink">
                 <a
+                  onClick={() => this.addTrainingData('white')}
                   target="_blank"
                   href={`http://ulanmedia.com/mgid/add-widgets-to-list-form.php?widgetIDs=${this.props.widgetRecord.widget_id.match(
                     /^\d*/,
@@ -75,6 +103,7 @@ class Record extends Component {
               </div>
               <div className="rowLink">
                 <a
+                  onClick={() => this.addTrainingData('grey')}
                   target="_blank"
                   href={`http://ulanmedia.com/mgid/add-widgets-to-list-form.php?widgetIDs=${this.props.widgetRecord.widget_id.match(
                     /^\d*/,
@@ -84,6 +113,7 @@ class Record extends Component {
               </div>
               <div className="rowLink">
                 <a
+                  onClick={() => this.addTrainingData('black')}
                   target="_blank"
                   href={`http://ulanmedia.com/mgid/add-widgets-to-list-form.php?widgetIDs=${this.props.widgetRecord.widget_id.match(
                     /^\d*/,
