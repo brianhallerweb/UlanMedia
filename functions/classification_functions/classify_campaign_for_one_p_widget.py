fbe638508e6ae14291c3f179f812cd63d0bbfd8a
Brian Haller <brianshaller@gmail.com>
2019-01-16 12:13:11 -0800

    commit

--- functions/classification_functions/classify_campaign_for_one_p_widget.py
+++ functions/classification_functions/classify_campaign_for_one_p_widget.py
@@ -1,22 +1,23 @@
 def classify_campaign_for_one_p_widget(campaign):
-    if campaign["clicks"] == 0:
-        return "wait"
-    elif campaign["leads"]/campaign["clicks"]*100 >= 0.20: 
-        if campaign["sales"] >= 1:
+    if campaign["clicks"] > 0:
+        if (campaign["sales"] > 0) & ((campaign["revenue"] - campaign["cost"]) > 0):
             return "good"
         else:
-            if campaign["leads"] >= 3:
-                return "good" 
+            if (campaign["leads"]/campaign["clicks"]*100) >= 0.20:
+                if campaign["leads"] >= 3:
+                    return "good" 
+                else:
+                    return "half good" 
             else:
-                return "half good" 
+                if (campaign["cost"] >= 30) | (campaign["clicks"] >= 700):
+                    return "bad"
+                elif (campaign["cost"] >= 10) & (campaign["cost"] <= 30):
+                    if campaign["leads"] == 0:
+                        return "half bad"
+                    else:
+                        return "wait"
+                else:
+                    return "wait"
     else:
-        if (campaign["cost"] > 30) | (campaign["clicks"] > 700):
-            return "bad"
-        elif ((campaign["cost"] > 10) & (campaign["cost"] <= 30)) & ((campaign["clicks"] > 300) & (campaign["clicks"] <= 700)):
-            if campaign["leads"] == 0:
-                return "half bad" 
-            else:
-                return "wait" 
-        elif ((campaign["cost"] < 10) | (campaign["clicks"] < 300)):
-            return "wait" 
+        return "wait"
 
