65571ccdc9b349264ae729e181e43680d3d56703
Brian Haller <brianshaller@gmail.com>
2019-02-02 10:34:49 -0800

    commit

--- functions/classification_functions/classify_campaign_for_one_p_widget.py
+++ functions/classification_functions/classify_campaign_for_one_p_widget.py
@@ -4,18 +4,25 @@ def classify_campaign_for_one_p_widget(campaign):
             return "good"
         else:
             if (campaign["leads"]/campaign["clicks"]*100) >= 0.20:
-                if campaign["leads"] >= 3:
+                if campaign["leads"] > 2:
                     return "good" 
                 else:
                     return "half good" 
             else:
-                if (campaign["cost"] >= 30) | (campaign["clicks"] >= 700):
+                if (campaign["cost"] >= 50) | (campaign["clicks"] >= 1000):
                     return "bad"
-                elif (campaign["cost"] >= 10) & (campaign["cost"] <= 30):
+                elif ((campaign["cost"] >= 30) & (campaign["cost"] <= 50)) & ((campaign["clicks"] >= 700) & (campaign["clicks"] <= 1000)):
+                    if campaign["leads"] == 0:
+                        return "bad"
+                    else: 
+                        return "half bad"
+                elif ((campaign["cost"] >= 10) & (campaign["cost"] <= 30)) & ((campaign["clicks"] >= 300) & (campaign["clicks"] <= 700)):
                     if campaign["leads"] == 0:
                         return "half bad"
-                    else:
+                    else: 
                         return "wait"
+                elif (campaign["cost"] <= 10) | (campaign["clicks"] <= 300):
+                    return "wait"
                 else:
                     return "wait"
     else:
--- functions/classification_functions/classify_p_widget_for_all_campaigns.py
+++ functions/classification_functions/classify_p_widget_for_all_campaigns.py
@@ -1,24 +1,33 @@
 def classify_p_widget_for_all_campaigns(p_widget):
-    if (p_widget["for_all_campaigns"]["cost"] >= 10) | (p_widget["for_all_campaigns"]["clicks"] >= 300):
-        if (p_widget["good_campaigns_count"] >= 3) & (p_widget["bad_campaigns_count"] == 0):
-            return "white"
-        elif (p_widget["good_campaigns_count"] == 0) & (p_widget["bad_campaigns_count"] >= 1) & (p_widget["for_all_campaigns"]["revenue"] - p_widget["for_all_campaigns"]["cost"] < -60):
-            return "black"
-        elif (p_widget["good_campaigns_count"] >= 3) & (p_widget["bad_campaigns_count"] >= 3):
+    if p_widget["for_all_campaigns"]["global_status"] == "blacklist":
+        return "black"
+    elif (p_widget["for_all_campaigns"]["cost"] >= 10) | (p_widget["for_all_campaigns"]["clicks"] >= 300):
+        #now I am adding new things
+        # grey
+        if (p_widget["for_all_campaigns"]["global_status"] == "greylist") & (p_widget["good_campaigns_count"] > 0):
             return "grey"
-        elif (p_widget["good_campaigns_count"] == 0) & (p_widget["bad_campaigns_count"] >= 3):
+        elif (p_widget["good_campaigns_count"] > 2) & (p_widget["bad_campaigns_count"] > 0):
+            return "grey"
+        # white
+        elif (p_widget["for_all_campaigns"]["global_status"] == "whitelist") & (p_widget["bad_campaigns_count"] == 0):
+            return "white"
+        elif (p_widget["good_campaigns_count"] > 2) & (p_widget["bad_campaigns_count"] == 0):
+            return "white"
+        # black 
+        elif (p_widget["good_campaigns_count"] == 0) & (p_widget["bad_campaigns_count"] > 2):
             return "black"
+        elif (p_widget["good_campaigns_count"] == 0) & (p_widget["bad_campaigns_count"] > 0) & (p_widget["for_all_campaigns"]["revenue"] - p_widget["for_all_campaigns"]["cost"] < -60):
+            return "black"
+        # wait
+        elif ((p_widget["good_campaigns_count"] > 0) & (p_widget["good_campaigns_count"] < 2)) | ((p_widget["bad_campaigns_count"] > 0) & (p_widget["bad_campaigns_count"] < 2)):
+            return "wait"
         else:
             return "wait"
     else:
-        if p_widget["for_all_campaigns"]["global_status"] == "not yet listed":
-            return "wait"
-        elif p_widget["for_all_campaigns"]["global_status"] == "blacklist":
-            return "black"
+        if p_widget["for_all_campaigns"]["global_status"] == "whitelist":
+            return "white"
         elif p_widget["for_all_campaigns"]["global_status"] == "greylist":
             return "grey"
-        elif p_widget["for_all_campaigns"]["global_status"] == "whitelist":
-            return "white"
         else:
             return "wait"
 
