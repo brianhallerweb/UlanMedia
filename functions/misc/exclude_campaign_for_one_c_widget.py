3328553e5d1b04da7085f47e69385e6002285c33
Brian Haller <brianshaller@gmail.com>
2019-02-16 17:19:27 -0800

    commit

--- functions/misc/exclude_campaign_for_one_c_widget.py
+++ functions/misc/exclude_campaign_for_one_c_widget.py
@@ -11,12 +11,12 @@ def exclude_campaign_for_one_c_widget(token, client_id, widget_id, campaign_id):
     res = requests.patch(url, 
         headers ={"Content-Type": "application/x-www-form-urlencoded",
             "Cache-Control": "no-cache"}, 
-          # data = {"widgetsFilterUid": f"include,except,{widget_id}"})
+          data = {"widgetsFilterUid": f"include,except,{widget_id}"})
           # 1/29/19 use the line below when you want to "include". I know it says
           # "exclude, except, widget_id", but these comment is correct, use the
           # line below if you want to include a campaign rather than exclude
           # it. Use the line above if you want to exclude a campaign. 
-         data = {"widgetsFilterUid": f"exclude,except,{widget_id}"})
+         # data = {"widgetsFilterUid": f"exclude,except,{widget_id}"})
     if res.status_code == 401:
         mgid_token = get_and_return_new_mgid_token()
         return exclude_campaign_for_one_c_widget(mgid_token, client_id, widget_id, campaign_id)
