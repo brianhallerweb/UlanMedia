3c0a7228909cf67fd89c4797cdad6d004572355d
Brian Haller <brianshaller@gmail.com>
2019-02-23 21:30:58 -0800

    commit

--- scripts/misc/test.py
+++ scripts/misc/test.py
@@ -1,7 +1,7 @@
 # import sys
 # print(sys.path)
 
-from scripts.functions.update_campaign_sets_file import update_campaign_sets_file
+from functions.misc.update_campaign_sets_file import update_campaign_sets_file
 
 print(update_campaign_sets_file())
 
