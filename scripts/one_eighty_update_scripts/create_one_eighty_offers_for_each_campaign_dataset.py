45e6d605d71833153c70f4812071790fa1c7a523
Brian Haller <brianshaller@gmail.com>
2019-03-19 12:29:45 -0700

    commit

--- scripts/one_eighty_update_scripts/create_one_eighty_offers_for_each_campaign_dataset.py
+++ scripts/one_eighty_update_scripts/create_one_eighty_offers_for_each_campaign_dataset.py
@@ -1,6 +1,6 @@
 from config.config import *
 from functions.data_acquisition_functions.get_vol_access_token import get_vol_access_token
-from functions.data_acquisition_functions.create_offers_dataset import create_offers_dataset
+from functions.data_acquisition_functions.create_offers_for_each_campaign_dataset import create_offers_for_each_campaign_dataset
 from functions.misc.create_vol_date_range import create_vol_date_range
 
 vol_token = get_vol_access_token(vol_access_id, vol_access_key)
@@ -10,7 +10,7 @@ vol_dates = create_vol_date_range(180, mgid_timezone)
 vol_start_date = vol_dates[0]
 vol_end_date = vol_dates[1]
 
-create_offers_dataset(vol_token, date_range, vol_start_date,
+create_offers_for_each_campaign_dataset(vol_token, date_range, vol_start_date,
         vol_end_date)
 
-print(f"{date_range} offers dataset created")
+print(f"{date_range} offers for each campaign dataset created")
