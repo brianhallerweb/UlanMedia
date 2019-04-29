0b514b4c095e9a4d011ec8e3b9d3dd5df419a752
Brian Haller <brianshaller@gmail.com>
2019-04-28 23:25:23 -0600

    commit

--- functions/data_acquisition_functions/get_mgid_included_widgets_by_campaign.py
+++ functions/data_acquisition_functions/get_mgid_included_widgets_by_campaign.py
@@ -1,7 +1,9 @@
 from config.config import *
 from config.mgid_token import mgid_token
 from functions.misc.get_and_return_new_mgid_token import get_and_return_new_mgid_token
+from functions.data_acquisition_functions.get_mgid_excluded_widgets_by_campaign import get_mgid_excluded_widgets_by_campaign
 import requests
+import sys
 
 def get_mgid_included_widgets_by_campaign(token, campaign_id, start_date, end_date):
     url =f"https://api.mgid.com/v1/goodhits/campaigns/{campaign_id}/quality-analysis?token={token}&campaignId={campaign_id}&dateInterval=interval&startDate={start_date}&endDate={end_date}";
@@ -12,17 +14,24 @@ def get_mgid_included_widgets_by_campaign(token, campaign_id, start_date, end_da
 
     response.raise_for_status()
     response = response.json()
-    included_widgets = []
+    widgets = []
     if response[campaign_id][start_date + "_" + end_date] == []:
-        return included_widgets
+        return widgets
     for id, data in response[campaign_id][start_date + "_" + end_date].items():
         widget_id = id
         if data["sources"]:
             for source_id, source_data in data["sources"].items():
                 if source_id is not "0":
                     widget_id = f"{id}s{source_id}"
-                included_widgets.append(widget_id)
+                    widgets.append(widget_id)
         else: 
-            included_widgets.append(widget_id)
+            widgets.append(widget_id)
+
+    excluded_widgets = get_mgid_excluded_widgets_by_campaign(token, mgid_client_id, campaign_id)
+    included_widgets = []
+    for widget in widgets:
+        if widget not in excluded_widgets:
+            included_widgets.append(widget)
     return included_widgets
+
 
