68cac37960241a84334a67539bf6ec53451469fe
Brian Haller <brianshaller@gmail.com>
2019-05-12 22:53:57 -0600

    commit

--- scripts/data_acquisition_scripts/create_days_for_one_p_widget_for_one_campaign_dataset.py
+++ scripts/data_acquisition_scripts/create_days_for_one_p_widget_for_one_campaign_dataset.py
@@ -4,10 +4,10 @@ from functions.misc.create_vol_date_range import create_vol_date_range
 from functions.data_acquisition_functions.create_days_for_one_p_widget_for_one_campaign_dataset import create_days_for_one_p_widget_for_one_campaign_dataset
 import sys
 
-# p_widget_id = sys.argv[1]
-# campaign_id = sys.argv[2]
-p_widget_id = "5679842 "
-campaign_id = "39eded96-4619-4c00-b0e6-bfdc622c894c"
+p_widget_id = sys.argv[1]
+campaign_id = sys.argv[2]
+# p_widget_id = "5679842"
+# campaign_id = "39eded96-4619-4c00-b0e6-bfdc622c894c"
 
 vol_token = get_vol_access_token(vol_access_id, vol_access_key)
 
