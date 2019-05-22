00fa2766e2a3249b90c7c5461cfcfb626dfe3dd3
Brian Haller <brianshaller@gmail.com>
2019-05-21 20:57:06 -0600

    commit

--- scripts/data_acquisition_scripts/create_months_for_one_country_for_all_campaigns_dataset.py
+++ scripts/data_acquisition_scripts/create_months_for_one_country_for_all_campaigns_dataset.py
@@ -4,8 +4,8 @@ from functions.misc.create_vol_date_range import create_vol_date_range
 from functions.data_acquisition_functions.create_months_for_one_country_for_all_campaigns_dataset import create_months_for_one_country_for_all_campaigns_dataset
 import sys
 
-# country_name = sys.argv[1]
-country_name = "south africa"
+country_name = sys.argv[1]
+# country_name = "south africa"
 
 vol_token = get_vol_access_token(vol_access_id, vol_access_key)
 
