2594b885455023a5b5184cd6af42851f8545f593
Brian Haller <brianshaller@gmail.com>
2019-06-05 17:21:14 -0600

    commit

--- functions/misc/create_widget_domain_lookup.py
+++ functions/misc/create_widget_domain_lookup.py
@@ -2,6 +2,7 @@ import requests
 import json
 import sys
 import os
+import re
 
 import pprint
 pp=pprint.PrettyPrinter(indent=2)
@@ -11,7 +12,15 @@ def create_widget_domain_lookup():
         domains = file.read().split("\n")
     
     widget_domain_lookup = {} 
-    
+
+    # domains = []
+    domain_pattern = re.compile(r'(.*//)([a-zA-Z0-9.]*)')
+    # for full_domain in full_domains:
+        # res = domain_pattern.findall(full_domain)
+        # if len(res) > 0:
+            # domain = res[0]
+            # domains.append(domain)
+
     n = 1
     for domain in domains:
         if len(domain) > 0:
@@ -23,12 +32,16 @@ def create_widget_domain_lookup():
                     traffic_source = row[0]
                     if traffic_source == "mgid.com":
                         widget_id = row[1]
-                        widget_domain_lookup[widget_id] = domain
+                        res = domain_pattern.findall(domain)
+                        if len(res) > 0:
+                            res = list(res[0])
+                            domain = res[1]
+                            widget_domain_lookup[widget_id] = domain
+
         n += 1
         if n > 50:
             break
             # pp.pprint(widget_domain_lookup)
-            # sys.exit()
 
     with open(f"../../data/widget_domain_lookup/widget_domain_lookup.json", "w") as file:
         json.dump(widget_domain_lookup, file)
