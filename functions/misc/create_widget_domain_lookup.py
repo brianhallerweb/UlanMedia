21b410f799ce0160dc337bf9e95afd80eb3888e7
Brian Haller <brianshaller@gmail.com>
2019-06-05 23:01:01 -0600

    commit

--- functions/misc/create_widget_domain_lookup.py
+++ functions/misc/create_widget_domain_lookup.py
@@ -8,40 +8,31 @@ import pprint
 pp=pprint.PrettyPrinter(indent=2)
 
 def create_widget_domain_lookup():
-    with open(f'{os.environ.get("ULANMEDIAAPP")}/data/domains_list/mgid_ads_dot_txt.txt', 'r') as file:
+    with open(f'{os.environ.get("ULANMEDIAAPP")}/data/ads.txt/ads.txt_list.txt', 'r') as file:
         domains = file.read().split("\n")
     
     widget_domain_lookup = {} 
 
-    # domains = []
     domain_pattern = re.compile(r'(.*//)([a-zA-Z0-9.]*)')
-    # for full_domain in full_domains:
-        # res = domain_pattern.findall(full_domain)
-        # if len(res) > 0:
-            # domain = res[0]
-            # domains.append(domain)
-
     n = 1
     for domain in domains:
         if len(domain) > 0:
-            res = requests.get(domain).text
-            lines = res.split("\n")
-            for line in lines:
-                row = line.split(", ")
-                if len(row) >= 2: 
-                    traffic_source = row[0]
-                    if traffic_source == "mgid.com":
-                        widget_id = row[1]
-                        res = domain_pattern.findall(domain)
-                        if len(res) > 0:
-                            res = list(res[0])
-                            domain = res[1]
-                            widget_domain_lookup[widget_id] = domain
-
-        n += 1
-        if n > 50:
-            break
-            # pp.pprint(widget_domain_lookup)
+            try:
+                res = requests.get(domain).text
+                lines = res.split("\n")
+                for line in lines:
+                    row = line.split(", ")
+                    if len(row) >= 2: 
+                        traffic_source = row[0]
+                        if traffic_source == "mgid.com":
+                            widget_id = row[1]
+                            res = domain_pattern.findall(domain)
+                            if len(res) > 0:
+                                res = list(res[0])
+                                domain = res[1]
+                                widget_domain_lookup[widget_id] = domain
+            except requests.exceptions.RequestException as e:
+                print(e)
 
     with open(f"../../data/widget_domain_lookup/widget_domain_lookup.json", "w") as file:
         json.dump(widget_domain_lookup, file)
