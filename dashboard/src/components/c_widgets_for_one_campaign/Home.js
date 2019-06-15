78771dc9b97919889ef806be6c74e91dca5512ed
Brian Haller <brianshaller@gmail.com>
2019-06-15 13:30:15 -0600

    commit

--- dashboard/src/components/c_widgets_for_one_campaign/Home.js
+++ dashboard/src/components/c_widgets_for_one_campaign/Home.js
@@ -28,7 +28,7 @@ class Home extends Component {
       c2Value: 'included',
       c3: false,
       c3Value: 'waiting',
-      c4: false,
+      c4: true,
       c4Value: 10,
       c5: false,
       c5Value: 10,
