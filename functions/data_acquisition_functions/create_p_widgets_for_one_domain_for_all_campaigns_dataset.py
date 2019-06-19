836b5f2f2fdec65b240abe4a6c934b979bfb298a
Brian Haller <brianshaller@gmail.com>
2019-06-18 22:03:41 -0600

    commit

--- functions/data_acquisition_functions/create_p_widgets_for_one_domain_for_all_campaigns_dataset.py
+++ functions/data_acquisition_functions/create_p_widgets_for_one_domain_for_all_campaigns_dataset.py
@@ -4,13 +4,12 @@ import sys
 import re
 import os
 
-import pprint
-pp=pprint.PrettyPrinter(indent=2)
+# import pprint
+# pp=pprint.PrettyPrinter(indent=2)
 
 def create_p_widgets_for_one_domain_for_all_campaigns_dataset(date_range, domain):
     
-    # 6/14 I'm not sure why I had this? But I think it is wrong
-    # domains = domain.split(",")
+    domains_to_check = domain.split(",")
 
     p_widgets_for_one_domain_for_all_campaigns = {"metadata": {"vol_start_date":
         "none", "vol_end_date":
@@ -20,11 +19,14 @@ def create_p_widgets_for_one_domain_for_all_campaigns_dataset(date_range, domain
         complete_p_widgets = json.load(file)
     
     for p_widget in complete_p_widgets:
-        if complete_p_widgets[p_widget]["for_all_campaigns"]["domain"] == domain:
-            p_widgets_for_one_domain_for_all_campaigns["data"][p_widget] = complete_p_widgets[p_widget]["for_all_campaigns"]
-            p_widgets_for_one_domain_for_all_campaigns["data"][p_widget]["good_campaigns_count"] = complete_p_widgets[p_widget]["good_campaigns_count"]
-            p_widgets_for_one_domain_for_all_campaigns["data"][p_widget]["bad_campaigns_count"] = complete_p_widgets[p_widget]["bad_campaigns_count"]
-            p_widgets_for_one_domain_for_all_campaigns["data"][p_widget]["wait_campaigns_count"] = complete_p_widgets[p_widget]["wait_campaigns_count"]
+        domains_to_match = complete_p_widgets[p_widget]["for_all_campaigns"]["domain"].split(",")
+        for domain_to_check in domains_to_check:
+            if domain_to_check in domains_to_match:
+                p_widgets_for_one_domain_for_all_campaigns["data"][p_widget] = complete_p_widgets[p_widget]["for_all_campaigns"]
+                p_widgets_for_one_domain_for_all_campaigns["data"][p_widget]["good_campaigns_count"] = complete_p_widgets[p_widget]["good_campaigns_count"]
+                p_widgets_for_one_domain_for_all_campaigns["data"][p_widget]["bad_campaigns_count"] = complete_p_widgets[p_widget]["bad_campaigns_count"]
+                p_widgets_for_one_domain_for_all_campaigns["data"][p_widget]["wait_campaigns_count"] = complete_p_widgets[p_widget]["wait_campaigns_count"]
+                break
 
 
     with open(f"{os.environ.get('ULANMEDIAAPP')}/data/p_widgets_for_one_domain_for_all_campaigns/{date_range}_{domain}_p_widgets_for_one_domain_for_all_campaigns_dataset.json", "w") as file:
